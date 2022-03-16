062685d960dd9354f4c99b0b4f05edf1e8f1647c
BT_1E10_09 <dariusj0714@gmail.com>
2022-03-16 13:46:33 -0400

    Initial commit

---
+++ src/Tablet.java
@@ -0,0 +1,16 @@
+public class Tablet extends Computer
+{
+    private boolean stylusIncluded;
+
+    public Tablet(int screenSize, int memory, boolean stylusIncluded)
+    {
+        super(screenSize, memory);
+        this.stylusIncluded = stylusIncluded;
+    }
+
+    public void installOperatingSystem()
+    {
+        System.out.println("installing OS...");
+    }
+}
+
